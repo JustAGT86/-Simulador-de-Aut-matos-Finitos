842bbbff929001b2a2e8c5ac17ff19571950e329
JustAGT86 <85511157+JustAGT86@users.noreply.github.com>
2023-01-30 13:12:10 -0300

    Add files via upload

--- AutomatoFinito.java
+++ AutomatoFinito.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class AutomatoFinito {
@@ -8,79 +7,76 @@ public class AutomatoFinito {
     public static void main(String[] args) {
         AutomatoFinito AUTOMATO= new AutomatoFinito();
         String palavra;
-        int i=0;
-        int j=0;
-        Scanner quantidade= new Scanner(System.in);
-        System.out.println("Digite a quantidade de vezes para o programar rodar: ");
-        j=quantidade.nextLine();
-        Scanner input= new Scanner(System.in);
-        System.out.println("Digite a palavra: ");
-        palavra=input.nextLine();
-
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Digite a palavra: ");
+            palavra=input.nextLine();
+        }
         AUTOMATO.cadeia=palavra.toCharArray();
         AUTOMATO.Iniciar();
     }
     
-        
-        public void Iniciar(){
-            cont=0;
-            q0();
+    
+    public void Iniciar(){
+        cont=0;
+        q0();
+    }
+    
+    public void q0(){
+        if(cont<cadeia.length){
+            if(cadeia[cont]=='a'){
+                cont++;
+                q1();
+            }             
+            else if(cadeia[cont] == 'c'){
+                qf();
+            }
+            else{
+                qerro();
+            }
+        }  
+
+        else{
+            qerro();
+        }
+    }
+
+    public void q1(){
+        if(cont<cadeia.length){
+            if(cadeia[cont]=='b'){
+                cont++;
+                q2();
+            }                                  
+            else{
+                qerro();
+            }
         }
 
-        public void q0(){
-            if(cont<cadeia.length){
-                if(cadeia[cont]=='a'){
-                    cont++;
-                    q1();
-                }             
-                else if(cadeia[cont] == 'c'){
-                    qf();
-                }
-                else{
-                    qerro();
-                }
-            }        
+        else{
+            qerro();
+        }
+    }
+
+    public void q2(){
+        if(cont<cadeia.length){
+            if(cadeia[cont]=='b'){
+                cont++;
+                q0();
+            }            
             else{
                 qerro();
             }
         }
         
-        public void q1(){
-            if(cont<cadeia.length){
-                if(cadeia[cont]=='b'){
-                    cont++;
-                    q2();
-                }                                   
-                else{
-                    qerro();
-                }
-            }        
-            else{
-                qerro();
-            }
+        else{
+            qerro();
         }
+    }
 
-        public void q2(){
-            if(cont<cadeia.length){
-                if(cadeia[cont]=='b'){
-                    cont++;
-                    qf();
-                }            
-                else{
-                    qerro();
-                }
-            }        
-            else{
-                qerro();
-            }
-        }
-
-        public void qf(){
-            System.err.println("1 (Palavra aceita pelo automato)");
-        }
-
-        public void qerro(){
-            System.err.println("0 (Palavra recusada pelo automato)");
-        }
-
+    public void qf(){
+        System.err.println("1");
+    }
+     
+    public void qerro(){
+        System.err.println("0");
+    }
 }
